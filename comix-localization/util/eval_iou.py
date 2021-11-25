de650fdc478c7df35b1b0ba82584eaf7c6bf8de8
Janghyun1230 <kimjanghyun1230@gmail.com>
2021-11-25 14:07:45 +0900

    clean up

--- comix-localization/util/eval_iou.py
+++ comix-localization/util/eval_iou.py
@@ -12,12 +12,12 @@ def eval_iou(b1, b2, mode="pascal"):
         x1, y1, w1, h1 = b1
         x2, y2, w2, h2 = b2
     elif mode == "pascal":
-        x1, y1, w1, h1 = b1[0], b1[1], b1[2]-b1[0], b1[3]-b1[1]
-        x2, y2, w2, h2 = b2[0], b2[1], b2[2]-b2[0], b2[3]-b2[1]
+        x1, y1, w1, h1 = b1[0], b1[1], b1[2] - b1[0], b1[3] - b1[1]
+        x2, y2, w2, h2 = b2[0], b2[1], b2[2] - b2[0], b2[3] - b2[1]
     xs = max(x1, x2)
     ys = max(y1, y2)
-    xe = min(x1+w1, x2+w2)
-    ye = min(y1+h1, y2+h2)
+    xe = min(x1 + w1, x2 + w2)
+    ye = min(y1 + h1, y2 + h2)
     intersection = (xe - xs) * (ye - ys)
-    union = w1*h1 + w2*h2 - intersection
+    union = w1 * h1 + w2 * h2 - intersection
     return intersection / union
